6db976e9f8c35eab1ca0441201ed5f6095d3ce8b
Pedro Mendonça <pgcalixto@gmail.com>
2018-11-15 05:05:17 -0200

    Add logic for simple string formatting
    Add the implementation for the logic of simple formatting:
    with limit capped at 40 characters and no justify.

--- strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
+++ strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
@@ -13,6 +13,34 @@ public class IdwallFormatter extends StringFormatter {
      */
     @Override
     public String format(String text) {
-        throw new UnsupportedOperationException();
+
+        // The regexp below splits the words separated by whitespace, but keeps
+        // the "\n" split as a single word.
+        // "Hello\n\n  World" would result in a ["Hello", "\n", "\n", "World"].
+        String[] words = text.split(" |((?<=\n)|(?=\n))");
+
+        StringBuilder sb = new StringBuilder();
+        int lineLength = 0;
+        for (String w : words) {
+            // If word is "\n", reset line length. Otherwise, if line is empty,
+            // add word. Otherwise, if new word does not overflow line, add
+            // whitespace + word. Otherwise, insert a new line and add word.
+            if (w.equals("\n")) {
+                lineLength = 0;
+            }
+            else if (lineLength == 0) {
+                lineLength += w.length();
+            }
+            else if (lineLength + w.length() + 1 <= 40) {
+                lineLength += w.length() + 1;
+                sb.append(' ');
+            }
+            else {
+                lineLength = w.length();
+                sb.append('\n');
+            }
+            sb.append(w);
+        }
+        return sb.toString();
     }
 }

8d9c7a792649b038fe6c2cafd26adf95790f6d97
Pedro Mendonça <pgcalixto@gmail.com>
2018-11-19 07:59:50 -0200

    Move format logic to private method
    A new method will be created in the future to format with
    justify. To better accomodate the logic for this, the
    formatting logic moved to a private method which returns
    a list of already formatted lines, with no justify, so that
    this method can be called by both format-with-no-justity
    and format-with-justify methods.

--- strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
+++ strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
@@ -1,18 +1,43 @@
 package idwall.desafio.string;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Rodrigo Catão Araujo on 06/02/2018.
  */
 public class IdwallFormatter extends StringFormatter {
 
     /**
-     * Should format as described in the challenge
+     * Formats text, with no justify, for the lines to meet the column cap as
+     * defined in the object instantiation.
      *
-     * @param text
-     * @return
+     * @param text Text to be formatted.
+     * @return Formatted text.
      */
     @Override
     public String format(String text) {
+        List<String> lines = this.getFormattedLines(text);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < lines.size() - 1; ++i) {
+            sb.append(lines.get(i));
+            sb.append("\n");
+        }
+        sb.append(lines.get(lines.size() - 1));
+
+        return sb.toString();
+    }
+
+    /**
+     * Format lines (without justify) and return them in a list of lines.
+     *
+     * @param text Text to be formatted
+     * @return List of already formatted lines
+     */
+    private List<String> getFormattedLines(String text) {
+
+        List<String> lines = new ArrayList<String>();
 
         // The regexp below splits the words separated by whitespace, but keeps
         // the "\n" split as a single word.
@@ -22,25 +47,32 @@ public class IdwallFormatter extends StringFormatter {
         StringBuilder sb = new StringBuilder();
         int lineLength = 0;
         for (String w : words) {
-            // If word is "\n", reset line length. Otherwise, if line is empty,
-            // add word. Otherwise, if new word does not overflow line, add
-            // whitespace + word. Otherwise, insert a new line and add word.
             if (w.equals("\n")) {
+                // when "\n", add the current built string to the list and reset
+                lines.add(sb.toString());
+                sb.setLength(0);
                 lineLength = 0;
             }
             else if (lineLength == 0) {
+                // beginning of line
+                sb.append(w);
                 lineLength += w.length();
             }
             else if (lineLength + w.length() + 1 <= 40) {
+                // word does not overflow line
+                sb.append(' ' + w);
                 lineLength += w.length() + 1;
-                sb.append(' ');
             }
             else {
+                // word overflows line
+                lines.add(sb.toString());
+                sb.setLength(0);
+                sb.append(w);
                 lineLength = w.length();
-                sb.append('\n');
             }
-            sb.append(w);
         }
-        return sb.toString();
+        // add last built string to the list and return
+        lines.add(sb.toString());
+        return lines;
     }
 }

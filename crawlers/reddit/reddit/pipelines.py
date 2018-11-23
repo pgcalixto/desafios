cc45e8a6bb94fabfd7c2ef135fdf1e75f84db936
Pedro Mendonça <pgcalixto@gmail.com>
2018-11-23 11:56:28 -0200

    Set pipeline to print table of extracted data

--- crawlers/reddit/reddit/pipelines.py
+++ crawlers/reddit/reddit/pipelines.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from tabulate import tabulate
 
 # Define your item pipelines here
 #
@@ -7,5 +8,28 @@
 
 
 class RedditPipeline(object):
+
+    def __init__(self):
+        self.threads = []
+        super().__init__()
+
     def process_item(self, item, spider):
+        self.threads.append(item)
         return item
+
+    def close_spider(self, spider):
+        headers = ['subreddit', 'upvotes', 'title', 'thread link', 'comments link']
+        data = []
+        for t in self.threads:
+            subreddit = t['subreddit']
+            upvotes = t['upvotes']
+            title = t['title'][:40] + (t['title'][40:] and '...')
+            thread_link = t['thread_link'][:50] + \
+                          (t['thread_link'][50:] and '...')
+            comments_link = t['comments_link'][:50] + \
+                            (t['comments_link'][50:] and '...')
+
+            data.append([subreddit, upvotes, title, thread_link,
+                         comments_link])
+
+        print(tabulate(data, headers=headers, tablefmt='grid'))

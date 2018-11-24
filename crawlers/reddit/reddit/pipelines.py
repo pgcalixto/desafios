e31268bb79c4d8d87833d0fe8225a0b32695de85
Pedro Mendonça <pgcalixto@gmail.com>
2018-11-24 09:56:30 -0200

    Output thread data to a file

--- crawlers/reddit/reddit/pipelines.py
+++ crawlers/reddit/reddit/pipelines.py
@@ -8,7 +8,6 @@ from tabulate import tabulate
 
 
 class RedditPipeline(object):
-
     def __init__(self):
         self.threads = []
         super().__init__()
@@ -18,7 +17,10 @@ class RedditPipeline(object):
         return item
 
     def close_spider(self, spider):
-        headers = ['subreddit', 'upvotes', 'title', 'thread link', 'comments link']
+        # CLI output
+        headers = [
+            'subreddit', 'upvotes', 'title', 'thread link', 'comments link'
+        ]
         data = []
         for t in self.threads:
             subreddit = t['subreddit']
@@ -29,7 +31,38 @@ class RedditPipeline(object):
             comments_link = t['comments_link'][:50] + \
                             (t['comments_link'][50:] and '...')
 
-            data.append([subreddit, upvotes, title, thread_link,
-                         comments_link])
+            data.append(
+                [subreddit, upvotes, title, thread_link, comments_link])
 
         print(tabulate(data, headers=headers, tablefmt='grid'))
+
+        # output to file for Telegram bot usage
+        with open('output.txt', 'w') as f:
+            for thread in self.threads[:-1]:
+                f.write(RedditPipeline.__format_thread_data(thread))
+                f.write('\n------------------------------\n')
+            f.write(RedditPipeline.__format_thread_data(self.threads[-1]))
+
+    @staticmethod
+    def __format_thread_data(thread):
+        '''Returns a formatted string with the thread data
+
+        Args:
+            thread (dict): Dictionary containing the thread information with
+                the following keys: subreddit, title, upvotes, thread_link and
+                comment_link.
+
+        Returns:
+            str: Formatted string, with one information per line.
+        '''
+        # TODO treat more carefully this error, possibly raising an exception.
+        if not all(
+                k in thread
+                for k in ('subreddit', 'title', 'upvotes', 'thread_link',
+                          'comments_link')):
+            return ''
+        return "Subreddit: " + thread['subreddit'] + \
+               "\nTitle: " + thread['title'] + \
+               "\nUpvotes: " + str(thread['upvotes']) + \
+               "\nThread link: " + thread['thread_link'] + \
+               "\nComments link: " + thread['comments_link']
